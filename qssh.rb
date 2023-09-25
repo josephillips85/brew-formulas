e02fbb00c54a9c83aa24b9eef6df392e8c47fb52
Jose Phillips <jose@latinol.com>
2023-09-24 22:40:05 -0500

    Replaced compile method

--- qssh.rb
+++ qssh.rb
@@ -8,24 +8,13 @@ class Qssh < Formula
     uses_from_macos "gcc"
   
     def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      system "/bin/bash build_mac.sh"
+      system "gcc qssh.c parser.c -o qssh"
       bin.install "qssh"
       
     end
   
     test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test qssh`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
+
       system "#{bin}/qssh", "version"
     end
   end
