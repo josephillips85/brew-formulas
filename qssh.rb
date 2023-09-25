c565128019d871c17481da5550b09cdb9e6833f5
Jose Phillips <jose@latinol.com>
2023-09-24 22:35:37 -0500

    fix

--- qssh.rb
+++ qssh.rb
@@ -11,7 +11,7 @@ class Qssh < Formula
       # ENV.deparallelize  # if your formula fails when building in parallel
       # Remove unrecognized options if warned by configure
       # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      system "./build_mac.sh"
+      system "build_mac.sh"
       bin.install "qssh"
       
     end
