61c22988c0ed51b4f5dc18e82dfe7053ba2dc6ac
Jose Phillips <jose@latinol.com>
2023-09-24 22:46:52 -0500

    First

--- qssh.rb
+++ qssh.rb
@@ -1,9 +1,9 @@
 class Qssh < Formula
-    desc "QuickSSH"
+    desc "qssh - QuickSSH streamlines server management for IT pros and developers, eliminating the need to remember multiple IP addresses and hostnames by providing easy access to a list of servers. "
     homepage "https://github.com/josephillips85/qssh/"
     url "https://github.com/josephillips85/qssh/archive/refs/tags/1.0.tar.gz"
     sha256 "d89802e374ec90ae3b6fa1017215cd6d04735c6b24d3e17a07c162b0832769fb"
-    license ""
+    license "GNU"
   
     uses_from_macos "gcc"
   
