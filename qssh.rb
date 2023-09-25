62c9f762a0affd7f542d34495f0fd5404bd6e433
Jose Phillips <jose@latinol.com>
2023-09-24 22:57:00 -0500

    Fix license

--- qssh.rb
+++ qssh.rb
@@ -3,7 +3,7 @@ class Qssh < Formula
     homepage "https://github.com/josephillips85/qssh/"
     url "https://github.com/josephillips85/qssh/archive/refs/tags/1.0.tar.gz"
     sha256 "d89802e374ec90ae3b6fa1017215cd6d04735c6b24d3e17a07c162b0832769fb"
-    license "GPL-3.0"
+    license "GPL-3.0-or-later"
   
     uses_from_macos "gcc"
   
