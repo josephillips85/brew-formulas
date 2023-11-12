f382b5f7983a4b2d96be378cc1c4715c68d3ffb1
Jose Phillips <jose@latinol.com>
2023-11-12 16:17:47 -0500

    add qssh 1.1

--- qsshAT1_0.rb
+++ qsshAT1_0.rb
@@ -1,8 +1,8 @@
-class Qssh < Formula
+class QsshAT1_0 < Formula
     desc "qssh - QuickSSH streamlines server management for IT pros and developers, eliminating the need to remember multiple IP addresses and hostnames by providing easy access to a list of servers. "
     homepage "https://github.com/josephillips85/qssh/"
-    url "https://github.com/josephillips85/qssh/archive/refs/tags/1.1.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    url "https://github.com/josephillips85/qssh/archive/refs/tags/1.0.tar.gz"
+    sha256 "d89802e374ec90ae3b6fa1017215cd6d04735c6b24d3e17a07c162b0832769fb"
     license "GPL-3.0-or-later"
   
     uses_from_macos "gcc"
