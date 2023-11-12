013f9b0b3522cff9adeecd453486487b17fa7ace
Jose Phillips <jose@latinol.com>
2023-11-12 16:19:32 -0500

    Fix sha256

--- qssh.rb
+++ qssh.rb
@@ -2,7 +2,7 @@ class Qssh < Formula
     desc "qssh - QuickSSH streamlines server management for IT pros and developers, eliminating the need to remember multiple IP addresses and hostnames by providing easy access to a list of servers. "
     homepage "https://github.com/josephillips85/qssh/"
     url "https://github.com/josephillips85/qssh/archive/refs/tags/1.1.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "a9ee5891e81d3f58b4a526e7941c6a669f053ff9524361a2f59d746d32fa9a54"
     license "GPL-3.0-or-later"
   
     uses_from_macos "gcc"
